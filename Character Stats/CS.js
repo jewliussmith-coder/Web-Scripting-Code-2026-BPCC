7f0c9bf949a41567d7c77f6b8c1403234e20fd0d
Jewlius Smith <jewlius.smith@gmail.com>
2026-08-27 14:40:40 -0500

    Week 2 Phase 3
    Week 2 Phase 3

--- Character Stats/CS.js
+++ Character Stats/CS.js
@@ -6,6 +6,8 @@ const BASECLASS = prompt("What's your class? (warrior or mage)");
 let currentHealth = 100;
 let experiencePoints = 0;
 
+//original power level
+let powerLevel = (currentHealth * 0.5) + (experiencePoints * 10);
 
 //print out the data from the variables
 console.log("Name: "+ NAME);
@@ -13,3 +15,24 @@ console.log("Homeworld: "+ HOMEWORLD);
 console.log("Class: "+ BASECLASS);
 console.log("Hp: "+ currentHealth);
 console.log("Exp: "+ experiencePoints);
+console.log("Power Level:" + powerLevel);
+
+//Find the health potion
+currentHealth += 20;
+experiencePoints += 50;
+
+//Power level should increase from that
+powerLevel = (currentHealth * 0.5) + (experiencePoints * 10);
+
+console.log("After Using Your Backpocket HP potion")
+console.log("Health" + currentHealth)
+console.log("Exp: "+ experiencePoints);
+console.log("Power Level:" + powerLevel);
+
+//TRAINING MONTAGE
+for (let day = 1; day <= 5; day++){
+    experiencePoints += 20;
+    console.log("Day "+ day + ": Training.... XP: " + experiencePoints)
+
+}
+console.log("After Training XP is now at: "+ experiencePoints)
