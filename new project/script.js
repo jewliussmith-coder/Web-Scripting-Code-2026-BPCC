b7d46728e306fb487dd30889b7e563d1bf7f40f6
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-03 14:00:20 -0500

    Phase 4

--- new project/script.js
+++ new project/script.js
@@ -16,4 +16,20 @@ function calculateStoreTax(amount) {
 
 console.log(calculateStoreTax(100));
 
-console.log(defaultTaxRate);
+// console.log(defaultTaxRate);
+
+
+function applyDiscount(total, discountCallback) {
+    return discountCallback(total);
+}
+
+function studentDiscount(total) {
+    return total * 0.90;
+}
+
+function seniorDiscount(total) {
+    return total * 0.80;
+}
+
+console.log(applyDiscount(100, studentDiscount));
+console.log(applyDiscount(100, seniorDiscount));
