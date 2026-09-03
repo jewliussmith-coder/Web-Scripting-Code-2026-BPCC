93d9b6c35c2d9f2d402e590bf5b401c3d5b2ae8d
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-03 13:55:08 -0500

    Phase 2 file
    Added a function to calculate store tax and log its result.

--- new project/script.js
+++ new project/script.js
@@ -11,3 +11,13 @@ let tax = getTax(subtotal, 0.07);
 
 console.log("Subtotal: $" + subtotal);
 console.log("Tax: $" + tax);
+
+
+function calculateStoreTax(amount) {
+    const defaultTaxRate = 0.07;
+    return getTax(amount, defaultTaxRate);
+}
+
+console.log(calculateStoreTax(100));
+
+console.log(defaultTaxRate);
