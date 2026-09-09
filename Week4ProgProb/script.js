05c66b2d16a889d700903c22e8b02ccc2e0aff33
Jewlius Smith <jewlius.smith@gmail.com>
2026-09-09 11:55:16 -0500

    Phase 2 file

--- Week4ProgProb/script.js
+++ Week4ProgProb/script.js
@@ -6,3 +6,26 @@ const storeInfo = {
 
 console.log(`Welcome to ${storeInfo.name}!`);
 console.log(`The standard table fee is $${storeInfo.tableFee.toFixed(2)}.`)
+
+const receiptItems = [];
+
+let continueOrdering = "Y";
+
+while (continueOrdering.toUpperCase() === "Y"){
+    const itemName = prompt("Enter the food item's name: ")
+    const itemPrice = Number(prompt("Enter the item's price: "))
+
+    const item = {
+        name: itemName,
+        price: itemPrice
+    };
+
+    receiptItems.push(item);
+
+    continueOrdering = prompt("Do you want to order another item? Enter Y to contine.")
+
+
+}
+
+console.log(`Total items: ${receiptItems.length}`)
+console.log("Receipt Item: ", receiptItems);
